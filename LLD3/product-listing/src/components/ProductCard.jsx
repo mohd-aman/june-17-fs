20c2980ff6ade6255aafa628052a3749545c5e99
aman <saifiamaan445@gmail.com>
2026-09-07 23:14:58 +0530

    7-sep-2026: event handler

--- LLD3/product-listing/src/components/ProductCard.jsx
+++ LLD3/product-listing/src/components/ProductCard.jsx
@@ -1,7 +1,26 @@
+import { useState } from "react";
+
 function ProductCard({ name, price, category, image }) {
   //   console.log(product);
   //   const { name, price, category, image } = product;
   //   console.log(props);
+  console.log("Re rendering")
+  const [quantity,setQuantity] = useState(0);
+  const [isFav,setIsFav] = useState(false);
+
+  function handleInc(incVal){
+    setQuantity(quantity+incVal);
+  }
+
+  function handleDec(){
+    if(quantity === 0) return;
+    setQuantity(quantity-1);
+  }
+
+  function handleFav(){
+    setIsFav(!isFav);
+  }
+
   return (
     <div
       style={{
@@ -13,8 +32,15 @@ function ProductCard({ name, price, category, image }) {
     >
       <img src={image} alt={name} style={{ width: "100%" }} />
       <h2>{name}</h2>
+      <button onClick={handleFav}>{isFav?"Remove from fav":"Add to Fav"}</button>
       <p>Category: {category}</p>
       <p>Price: {price}</p>
+      <div style={{ display: "flex", alignItems: "center", gap: "10px", marginTop: "10px" }}>
+        <button onClick={handleDec}>-</button>
+        <span>{quantity}</span>
+        <button onClick={function(){handleInc(20)}}>+</button>
+         <button onClick={()=>handleInc(20)}></button>
+      </div>
     </div>
   );
 }
